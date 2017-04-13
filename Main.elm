75e01d0314ac32f8ed34bbf1dc65fa71b24904b2
Tom Wey <tjmwey@gmail.com>
2017-04-13 20:15:05 +0100

    Initial commit

---
+++ Main.elm
@@ -0,0 +1,56 @@
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+-- MODEL
+
+type alias Model = { ... }
+
+
+model : Model
+model =
+    {
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( model, Cmd.none )
+
+
+-- UPDATE
+
+type Msg = Reset | ...
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Reset -> ...
+    ...
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  ...
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [
+        ]
